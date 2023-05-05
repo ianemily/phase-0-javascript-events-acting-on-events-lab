64c5f4adced0e27e4594c347f973e385dcde9d42
Emily Iannizzi <eiannizzi18@gmail.com>
2023-05-05 15:52:03 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+const DODGER = document.getElementById('dodger')
+
+
+function moveDodgerLeft() {
+    const left = parseInt(DODGER.style.left)
+    if (left > 0) {
+      DODGER.style.left = `${left - 4}px`
+    }
+  }
+  
+  function moveDodgerRight() {
+    const left = parseInt(DODGER.style.left)
+    if (left < 360) {
+      DODGER.style.left = `${left + 4}px`
+    }
+  }
+  
